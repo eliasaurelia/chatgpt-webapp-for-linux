aeb05f2ffd29523e4db629dd7696fdaf9d8d9633
eliasaurelia <kaizephyr@proton.me>
2026-06-14 12:17:01 +0800

    chore: update electron dependencies

--- electron-builder.config.cjs
+++ electron-builder.config.cjs
@@ -3,7 +3,7 @@ const path = require('node:path');
 module.exports = {
   appId: 'local.chatgpt.webapp.linux',
   productName: 'ChatGPT WebApp',
-  electronVersion: '42.3.3',
+  electronVersion: '42.4.0',
   npmRebuild: false,
   directories: {
     output: path.join(__dirname, 'release'),
